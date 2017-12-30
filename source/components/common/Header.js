481c74f7deeec429bada01eed916179aff8d2071
dornellaskj <dornellaskj@gmail.com>
2017-12-30 09:28:51 -0700

    added mobile nav

--- source/components/common/Header.js
+++ source/components/common/Header.js
@@ -6,11 +6,19 @@ class Header extends Component {
     return (
       <header className="header">
         <h1>Kevin D'Ornellas</h1>
-        <nav >
-          <a href="/">Home</a>
-          <a href="/resume/">Resume</a>
-          <a href="/works/">Works</a>
-          <a href="/contact/">Contact</a>
+        <nav>
+        <input type="checkbox" id="checkbox_toggle"/>
+          <div className="nav-container">
+            <a href="/">Home</a>
+            <a href="/resume/">Resume</a>
+            <a href="/works/">Works</a>
+            <a href="/contact/">Contact</a>
+          </div>
+          <label htmlFor="checkbox_toggle" className="hamburger">
+              <div className="line line-1"></div>
+              <div className="line line-2"></div>
+              <div className="line line-3"></div>
+          </label>
         </nav>
       </header>
     );
