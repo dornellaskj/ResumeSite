6d7bf60fcb9f5bdba8b8e112d16e8d4bca5d5af0
Kevin D'Ornellas <dornellaskj@gmail.com>
2018-05-20 11:58:35 -0600

    adding a few SEO meta tags

--- client/src/headConfigs/resume.js
+++ client/src/headConfigs/resume.js
@@ -2,10 +2,15 @@ module.exports.default =
 {
 	"title": "Kevin Dornellas | Resume Page",
 	meta: [
-		{ name: "viewport", content: "width=device-width,initial-scale=1" }
+		{ name: "viewport", content: "width=device-width,initial-scale=1" },
+		{ property:"og:description", content:"Kevin Dornellas, a senior software developer with many talents and 14 years experience."},
+		{ property:"og:title", content:"Kevin Dornellas | Resume"},
+		{ property:"og:image", content:""},
+		{ property:"og:site_name",	content:"kevindornellas.com"}
+
 	],
 	scss: [
-		{rel:"canonical", href:"http://www.kevindornellas.com/"},
+		{rel:"canonical", href:"http://www.kevindornellas.com/resume/"},
 		{rel:"stylesheet", type:"text/css", href:"/index.css"}
 	]
 };
