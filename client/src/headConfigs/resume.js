c85e1deee7bad96adc628bede47fd6566b7aa445
Kevin D'Ornellas <dornellaskj@gmail.com>
2018-05-20 12:57:46 -0600

    adding description

--- client/src/headConfigs/resume.js
+++ client/src/headConfigs/resume.js
@@ -3,6 +3,7 @@ module.exports.default =
 	"title": "Kevin Dornellas | Resume Page",
 	meta: [
 		{ name: "viewport", content: "width=device-width,initial-scale=1" },
+		{ name: "description", content: "Kevin Dornellas, a senior software developer with many talents and 14 years experience." },
 		{ property:"og:description", content:"Kevin Dornellas, a senior software developer with many talents and 14 years experience."},
 		{ property:"og:title", content:"Kevin Dornellas | Resume"},
 		{ property:"og:image", content:""},
