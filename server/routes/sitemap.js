b3881469232897f3ebe3b9bb30274c0dd0cac1fd
Kevin D'Ornellas <dornellaskj@gmail.com>
2018-05-20 13:09:00 -0600

    update to sitemap file

--- server/routes/sitemap.js
+++ server/routes/sitemap.js
@@ -10,12 +10,10 @@ export default (req, res) => {
 		});
 		res.end();
 	} else {
-		res.status(200).send(renderFullPage());
+		res.status(200).send(page);
 	}
 };
-function renderFullPage() {
-	return `
-	<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+let page =  `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
 	<url>
 	<loc>http://www.kevindornellas.com/</loc>
 	<lastmod>2018-05-20T19:01:16+00:00</lastmod>
@@ -41,6 +39,4 @@ function renderFullPage() {
 	<lastmod>2018-05-20T19:01:16+00:00</lastmod>
 	<priority>0.64</priority>
 	</url>
-	</urlset>
-          `;
-}
+	</urlset>`;
