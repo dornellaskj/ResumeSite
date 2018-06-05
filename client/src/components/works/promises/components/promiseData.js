c258cba1f440c2b05465c2884919bd9651764a13
dornellaskj <dornellaskj@gmail.com>
2018-06-05 08:10:33 -0600

    added to promise tutorial.

--- client/src/components/works/promises/components/promiseData.js
+++ client/src/components/works/promises/components/promiseData.js
@@ -1,14 +1,14 @@
 export default [
   {
     code: `const promise1 = new Promise(function(resolve, reject) {
-			setTimeout(resolve, 100, 'foo');
+			setTimeout(resolve, 200, 'foo');
 		});
 		
 		promise1.then( result => {
 			console.log(result);
 		});
 		
-    console.log("=-", promise1);`,
+    console.log("promise before resolution:", promise1);`,
     title: "Example 1: Race condition",
     overview: `A promise will wait to execute the code within until after the resolution. This is necessary because of Javascript's 
     non blocking nature. These are most often used for network calls or for complicated methods that would cause a race condition
@@ -16,14 +16,81 @@ export default [
   },
   {
     code: `const promise1 = new Promise(function(resolve, reject) {
-			setTimeout(resolve, 100, 'foo');
+			setTimeout(resolve, 400, 'never gonna give you up');
+    });
+    
+    const promise2 = new Promise(function(resolve, reject) {
+			setTimeout(resolve, 400, 'never gonna let you down');
+    });
+    
+    const promise3 = new Promise(function(resolve, reject) {
+			setTimeout(resolve, 400, 'never gonna turn around and desert you');
 		});
 		
 		promise1.then( result => {
       console.log(result);
-      console.log("what up!!!");
-		});`,
+      return promise2;
+		}).then( result => {
+      console.log(result);
+      return promise3;
+    }).then(result => {
+      console.log(result);
+    });`,
     title: "Example 2: Chained Promises",
-    overview: `Sometimes a promise is dependent on the result from another promise.`
+    overview: `Sometimes a promise is dependent on the result from another promise. In this case you will need to chain your promises
+    together, to be sure that they will execute in the correct order.`
+  },
+  {
+    code: `const promise1 = new Promise(function(resolve, reject) {
+			setTimeout(resolve, 400, 'never gonna make you cry');
+    });
+    
+    const promise2 = new Promise(function(resolve, reject) {
+			setTimeout(resolve, 400, 'never gonna say goodbye');
+    });
+    
+    const promise3 = new Promise(function(resolve, reject) {
+			setTimeout(resolve, 400, 'never gonna tell a lie or hurt you');
+		});
+    
+    let promiseArray = [promise1, promise2, promise3];
+
+    Promise.all(promiseArray).then(resultArray => {
+      resultArray.forEach(result => {
+        console.log(result);
+      });
+    });
+		`,
+    title: "Example 3: Promise Async",
+    overview: `If you have many promises to resolve that are not dependent on each other you can resolve them asynchronously with a promise all shown below. The promise
+    all will take in an array of promises and return an array of the responses in the same order.`    
+  },
+  {
+    code: `const promise1 = new Promise(function(resolve, reject) {
+			setTimeout(reject, 200, 'never gonna make you cry');
+    }).catch( ()=> {
+      return 'Ricky Astley broken...';
+    });
+    
+    const promise2 = new Promise(function(resolve, reject) {
+			setTimeout(resolve, 400, 'never gonna say goodbye');
+    });
+    
+    const promise3 = new Promise(function(resolve, reject) {
+			setTimeout(resolve, 400, 'never gonna tell a lie or hurt you');
+		});
+    
+    let promiseArray = [promise1, promise2, promise3];
+    
+    Promise.all(promiseArray).then(resultArray => {
+      resultArray.forEach(result => {
+        console.log(result);
+      });
+    });
+		`,
+    title: "Example 4: Promise Error handling",
+    overview: `let's say you are running a promise all, but one of the promises fails... What then? You can catch each promise individually and handle the failure gacefully
+    ensuring that if the other promises reolve you still have the result from those. As seen in the example below promise1 will fail, and the catch will fire. The promise all 
+    will still execute.`    
   }
 ];
